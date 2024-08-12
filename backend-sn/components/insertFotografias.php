f0554e924a6dfbd60ab0e482b772a0849e2a8d09
tioya2923 <30002076@students.ual.pt>
2024-08-12 22:44:50 +0100

    meter pasta

--- backend-sn/components/insertFotografias.php
+++ backend-sn/components/insertFotografias.php
@@ -6,12 +6,7 @@ require_once '../connect/cors.php';
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 
-
-
 $bucketName = 'retratos-paroquia-sao-nicolau';
-
-
-
 $IAM_KEY = getenv('AWS_ACCESS_KEY_ID');
 $IAM_SECRET = getenv('AWS_SECRET_ACCESS_KEY');
 
@@ -24,35 +19,38 @@ $s3 = new S3Client([
     ],
 ]);
 
+if (isset($_FILES['image'])) {
+    foreach ($_FILES['image']['error'] as $key => $error) {
+        if ($error === UPLOAD_ERR_OK) {
+            $image = $_FILES['image'];
+            $filename = mysqli_real_escape_string($conn, $image['name'][$key]);
+            $nome = isset($_POST['nome']) ? mysqli_real_escape_string($conn, $_POST['nome']) : 'Nome padrão';
+            $descricao = isset($_POST['descricao']) ? mysqli_real_escape_string($conn, $_POST['descricao']) : 'Descrição padrão';
+            $pasta = isset($_POST['pasta']) ? mysqli_real_escape_string($conn, $_POST['pasta']) : 'pasta_padrao';
+            
+            try {
+                $key = "uploads/{$pasta}/{$filename}";
+                $result = $s3->putObject([
+                    'Bucket' => $bucketName,
+                    'Key'    => $key,
+                    'SourceFile' => $image['tmp_name'][$key],
+                    'ACL'    => 'public-read',
+                    'ContentType' => 'image/png'
+                ]);
 
+                $stmt = $conn->prepare("INSERT INTO fotos (nome, foto, descricao) VALUES (?, ?, ?)");
+                $stmt->bind_param("sss", $nome, $key, $descricao);
 
-if (isset($_FILES['image']) && $_FILES['image']['error'] === 0) {
-    $image = $_FILES['image'];
-    $filename = mysqli_real_escape_string($conn, $image['name']);
-    $nome = isset($_POST['nome']) ? mysqli_real_escape_string($conn, $_POST['nome']) : 'Nome padrão';
-    $descricao = isset($_POST['descricao']) ? mysqli_real_escape_string($conn, $_POST['descricao']) : 'Descrição padrão';
-    
-    try {
-        $key = "uploads/{$filename}";
-        $result = $s3->putObject([
-            'Bucket' => $bucketName,
-            'Key'    => $key,
-            'SourceFile' => $image['tmp_name'],
-            'ACL'    => 'public-read',
-            'ContentType' => 'image/png'
-        ]);
-
-        $stmt = $conn->prepare("INSERT INTO fotos (nome, foto, descricao) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $nome, $key, $descricao);
-
-        if ($stmt->execute()) {
-            echo "Foto enviada com sucesso!";
-        } else {
-            echo "Erro ao enviar a foto: " . $stmt->error;
+                if ($stmt->execute()) {
+                    echo "Foto enviada com sucesso!";
+                } else {
+                    echo "Erro ao enviar a foto: " . $stmt->error;
+                }
+                $stmt->close();
+            } catch (S3Exception $e) {
+                echo "Houve um erro ao fazer upload no S3: " . $e->getMessage();
+            }
         }
-        $stmt->close();
-    } catch (S3Exception $e) {
-        echo "Houve um erro ao fazer upload no S3: " . $e->getMessage();
     }
 } else {
     echo "Nenhuma fotografia selecionada ou erro no arquivo.";
