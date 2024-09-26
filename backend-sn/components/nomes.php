6d2fc34ffb0b522f25e237f2f7269b9190f2978c
Joao Dumba <30002076@students.ual.pt>
2024-09-27 00:55:49 +0100

    data

--- backend-sn/components/nomes.php
+++ backend-sn/components/nomes.php
@@ -8,15 +8,21 @@ if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
+// Definir o fuso horário para Portugal Continental
+date_default_timezone_set('Europe/Lisbon');
+
 // Buscar Nomes e Datas de Aniversário
 $sql = "SELECT nome_completo, data_aniversario, data_aniversario_sacerdotal FROM nomes";
 $result = $conn->query($sql);
 $nomes = [];
 while ($row = $result->fetch_assoc()) {
+    $dataAniversario = $row['data_aniversario'] ? new DateTime($row['data_aniversario']) : null;
+    $dataAniversarioSacerdotal = $row['data_aniversario_sacerdotal'] ? new DateTime($row['data_aniversario_sacerdotal']) : null;
+    
     $nomes[] = [
         'nome_completo' => $row['nome_completo'],
-        'data_aniversario' => $row['data_aniversario'],
-        'data_aniversario_sacerdotal' => $row['data_aniversario_sacerdotal']
+        'data_aniversario' => $dataAniversario ? $dataAniversario->format('Y-m-d H:i:s') : null,
+        'data_aniversario_sacerdotal' => $dataAniversarioSacerdotal ? $dataAniversarioSacerdotal->format('Y-m-d H:i:s') : null
     ];
 }
 $conn->close();
