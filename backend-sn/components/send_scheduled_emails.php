4cb3856d490422c272fefb95f6aff54a8e54fc9a
Joao Dumba <30002076@students.ual.pt>
2024-11-19 18:33:07 +0000

    Lembrete123456789123691119144

--- backend-sn/components/send_scheduled_emails.php
+++ backend-sn/components/send_scheduled_emails.php
@@ -9,10 +9,6 @@ require_once '../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
-
 function sendEmail($subject, $body, $recipients) {
     $mail = new PHPMailer(true);
     try {
@@ -24,7 +20,7 @@ function sendEmail($subject, $body, $recipients) {
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
 
-        $mail->setFrom('retratospsn@gmail.com', utf8_decode('Paróquia de São Nicolau'));
+        $mail->setFrom('retratospsn@gmail.com', 'Paróquia de São Nicolau');
 
         foreach ($recipients as $recipient) {
             $mail->addAddress($recipient);
@@ -58,8 +54,6 @@ if ($result->num_rows > 0) {
 
 while (true) {
     $currentDayOfWeek = date('N');
-    $currentHour = date('G');
-    $currentMinute = date('i');
     $currentTime = date('H:i');
 
     echo "Verificando emails a serem enviados. Data e Hora atuais: " . date('Y-m-d H:i:s') . "\n";
@@ -68,8 +62,8 @@ while (true) {
     
     // Definir os horários para envio dos emails
     $timesToCheck = [
-        '2-18:30' => ['subject' => 'Bom dia!', 'body' => '<p>Olá, Bom dia! Preparado para mais uma semana laboral?</p><p>Passo apenas para lhe fazer lembrar o seguinte: INSCREVA-TE PARA AS REFEIÇÕES.</p> <p>São Nicolau agradece!</p>'],
-        '4-21:00' => ['subject' => 'Boa noite!', 'body' => '<p>Olá, boa noite! Como está a decorrer a tua semana laboral?</p><p>Se ainda não fez a inscrição para as refeições, faça-o agora mesmo.</a></p> <p>São Nicolau agradece!</p>'],
+        '2-18:40' => ['subject' => 'Bom dia!', 'body' => '<p>Olá, Bom dia! Preparado para mais uma semana laboral?</p><p>Passo apenas para lhe fazer lembrar o seguinte: INSCREVA-TE PARA AS REFEIÇÕES.</p> <p>São Nicolau agradece!</p>'],
+        '4-21:00' => ['subject' => 'Boa noite!', 'body' => '<p>Olá, boa noite! Como está a decorrer a tua semana laboral?</p><p>Se ainda não fez a inscrição para as refeições, faça-o agora mesmo.</p> <p>São Nicolau agradece!</p>'],
         '6-14:30' => ['subject' => 'Boa tarde!', 'body' => '<p>Olá, boa tarde!</p><p>Aproveite o final de semana para fazer a inscrição para as refeições.</p> <p>São Nicolau agradece!</p>']
     ];
 
@@ -86,5 +80,5 @@ while (true) {
     sleep(60);
 }
 
-
+$conn->close();
 ?>
