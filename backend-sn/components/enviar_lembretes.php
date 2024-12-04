590926d2f91f83c5f0b42571f7fbeafc6430db4b
Joao Dumba <30002076@students.ual.pt>
2024-12-04 20:14:20 +0000

    Adicionado script para enviar lembretes

--- backend-sn/components/enviar_lembretes.php
+++ backend-sn/components/enviar_lembretes.php
@@ -9,74 +9,81 @@ require_once '../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-// Carregar emails dos usuários
-$sql = "SELECT email FROM usuarios";
-$result = $conn->query($sql);
-$emails = [];
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $emails[] = $row['email'];
-    }
-    echo "Emails carregados com sucesso.\n";
-} else {
-    echo "Nenhum email encontrado.\n";
-}
+// Função para enviar lembretes
+function enviarLembretes() {
+    global $conn;
 
-// Função para enviar emails
-function sendEmail($subject, $body, $recipients) {
-    $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'retratospsn@gmail.com';
-        $mail->Password = 'thqyngnejodzttwl'; 
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->Port = 465;
-
-        $mail->setFrom('retratospsn@gmail.com', 'Paróquia de São Nicolau');
-
-        foreach ($recipients as $recipient) {
-            $mail->addAddress($recipient);
+    // Carregar emails dos usuários
+    $sql = "SELECT email FROM usuarios";
+    $result = $conn->query($sql);
+    $emails = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $emails[] = $row['email'];
         }
-
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->Body = $body;
-        $mail->AltBody = strip_tags($body);
-
-        $mail->send();
-        echo "Email enviado com sucesso para todos os destinatários.\n";
-    } catch (Exception $e) {
-        error_log($e->getMessage());
-        echo "Erro ao enviar email: " . $e->getMessage() . "\n";
+        echo "Emails carregados com sucesso.\n";
+    } else {
+        echo "Nenhum email encontrado.\n";
     }
+
+    // Função para enviar emails
+    function sendEmail($subject, $body, $recipients) {
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'retratospsn@gmail.com';
+            $mail->Password = 'thqyngnejodzttwl';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = 465;
+
+            $mail->setFrom('retratospsn@gmail.com', 'Paróquia de São Nicolau');
+
+            foreach ($recipients as $recipient) {
+                $mail->addAddress($recipient);
+            }
+
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            $mail->AltBody = strip_tags($body);
+
+            $mail->send();
+            echo "Email enviado com sucesso para todos os destinatários.\n";
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            echo "Erro ao enviar email: " . $e->getMessage() . "\n";
+        }
+    }
+
+    // Agendamento de envio de emails
+    $currentDayOfWeek = date('N');
+    $currentTime = date('H:i');
+
+    echo "Verificando emails a serem enviados. Data e Hora atuais: " . date('Y-m-d H:i:s') . "\n";
+    echo "Dia atual: $currentDayOfWeek\n";
+    echo "Hora atual: $currentTime\n";
+
+    // Definir os horários para envio dos emails
+    $timesToCheck = [
+        '2-09:00' => ['subject' => 'Bom dia!', 'body' => '<p>Olá, Bom dia! Preparado para mais uma semana laboral?</p><p>Passo apenas para lhe fazer lembrar o seguinte: INSCREVA-TE PARA AS REFEIÇÕES.</p> <p>São Nicolau agradece!</p>'],
+        '5-21:30' => ['subject' => 'Boa noite!', 'body' => '<p>Olá, boa noite! Como está a decorrer a tua semana laboral?</p><p>Se ainda não fez a inscrição para as refeições, faça-o agora mesmo.</p> <p>São Nicolau agradece!</p>'],
+        '7-14:30' => ['subject' => 'Boa tarde!', 'body' => '<p>Olá, boa tarde!</p><p>Aproveite o final de semana para fazer a inscrição para as refeições.</p> <p>São Nicolau agradece!</p>']
+    ];
+
+    // Verificar se é o horário de envio de email
+    foreach ($timesToCheck as $dayTime => $emailData) {
+        list($day, $time) = explode('-', $dayTime);
+        if ($currentDayOfWeek == $day && $currentTime == $time) {
+            echo "Enviando email para: $currentTime\n";
+            sendEmail($emailData['subject'], $emailData['body'], $emails);
+        }
+    }
+
+    $conn->close();
 }
 
-// Agendamento de envio de emails
-$currentDayOfWeek = date('N');
-$currentTime = date('H:i');
-
-echo "Verificando emails a serem enviados. Data e Hora atuais: " . date('Y-m-d H:i:s') . "\n";
-echo "Dia atual: $currentDayOfWeek\n";
-echo "Hora atual: $currentTime\n";
-
-// Definir os horários para envio dos emails
-$timesToCheck = [    
-    '2-09:00' => ['subject' => 'Bom dia!', 'body' => '<p>Olá, Bom dia! Preparado para mais uma semana laboral?</p><p>Passo apenas para lhe fazer lembrar o seguinte: INSCREVA-TE PARA AS REFEIÇÕES.</p> <p>São Nicolau agradece!</p>'],
-    '4-18:00' => ['subject' => 'Bom tarde!', 'body' => '<p>Olá, Boa tarde! </p><p>Já fez a inscrição para as refeições?</p> <p>São Nicolau agradece!</p>'],
-    '5-21:30' => ['subject' => 'Boa noite!', 'body' => '<p>Olá, boa noite! Como está a decorrer a tua semana laboral?</p><p>Se ainda não fez a inscrição para as refeições, faça-o agora mesmo.</p> <p>São Nicolau agradece!</p>'],
-    '7-14:30' => ['subject' => 'Boa tarde!', 'body' => '<p>Olá, boa tarde!</p><p>Aproveite o final de semana para fazer a inscrição para as refeições.</p> <p>São Nicolau agradece!</p>']
-];
-
-// Verificar se é o horário de envio de email
-foreach ($timesToCheck as $dayTime => $emailData) {
-    list($day, $time) = explode('-', $dayTime);
-    if ($currentDayOfWeek == $day && $currentTime == $time) {
-        echo "Enviando email para: $currentTime\n";
-        sendEmail($emailData['subject'], $emailData['body'], $emails);
-    }
-}
-
-$conn->close();
+// Chamar a função para enviar lembretes
+enviarLembretes();
 ?>
