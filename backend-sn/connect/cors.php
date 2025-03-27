e2931dd3e3aad1206421703ed387baa42095a6c9
Joao Dumba <30002076@students.ual.pt>
2025-03-27 01:19:14 +0000

    mudanac

--- backend-sn/connect/cors.php
+++ backend-sn/connect/cors.php
@@ -3,20 +3,22 @@
 $origensPermitidas = [
     'http://snrefeicoes.pt',
     'http://www.snrefeicoes.pt',
-    'http://localhost:3000' // Caso você esteja testando localmente
+    'http://localhost:3000'
 ];
 
 // Verifica se a origem da requisição está na lista de permitidas
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $origensPermitidas)) {
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
     header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Allow-Methods: OPTIONS, PATCH, DELETE, POST, PUT, GET');
-    header('Access-Control-Allow-Headers: X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
-
+    header('Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        // Finaliza a requisição para preflight de métodos OPTIONS
+        // Retorna sem executar mais nada para as requisições OPTIONS preflight
         exit(0);
     }
 }
+
+// Seu código de processamento normal aqui
 header('Content-Type: application/json');
 ?>
