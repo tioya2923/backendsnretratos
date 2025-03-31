d3166a191f1fd2babe452c79bb7a32cc7204ae86
Joao Dumba <30002076@students.ual.pt>
2025-03-31 12:02:13 +0100

    Adicionando arquivos não rastreados1

--- backend-sn/connect/server.php
+++ backend-sn/connect/server.php
@@ -1,50 +1,42 @@
 <?php
-// Importar a biblioteca Dotenv
-require_once __DIR__ . '/cors.php'; // Caminho relativo ao diretório atual
+require_once __DIR__ . '/../cors.php'; 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-#use Dotenv;
-
-// Carregar variáveis do arquivo .env
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
-$dotenv->load();
-
-// Testar se o Dotenv carregou corretamente
-//var_dump($_ENV['DB_URL']);
-//var_dump(getenv('DB_URL'));
-
-// Acessar a variável DB_URL
-//$dbUrl = getenv('DB_URL');
-$dbUrl = $_ENV['DB_URL'];
-
-//var_dump($_ENV['DB_URL']);
-//var_dump(getenv('DB_URL'));
-
-if (!$dbUrl) {
-    die("A variável de ambiente DB_URL não foi carregada corretamente.");
+try {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 3));
+    $dotenv->load();
+} catch (Dotenv\Exception\InvalidPathException $e) {
+    die(json_encode(['error' => 'O arquivo .env não foi encontrado.']));
 }
 
-// Continuar com o código do CORS
-$origensPermitidas = [
-    'http://snrefeicoes.pt',
-    'http://www.snrefeicoes.pt',
-    'http://localhost:3000',
-    'http://135.181.47.213'
-];
+$clearDbUrl = getenv('DB_URL');
 
-// Verificar se a origem da requisição está na lista de permitidas
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $origensPermitidas)) {
-    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-    
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        // Retorna sem executar mais nada para as requisições OPTIONS preflight
-        exit(0);
+if (!$clearDbUrl) {
+    die(json_encode(['error' => 'A variável DB_URL não foi configurada.']));
+}
+
+$url = parse_url($clearDbUrl);
+
+if (!isset($url["host"], $url["user"], $url["pass"], $url["path"])) {
+    die(json_encode(['error' => 'URL de conexão ao banco está incompleta.']));
+}
+
+$host = $url["host"];
+$user = $url["user"];
+$password = $url["pass"];
+$db = ltrim($url["path"], '/');
+
+define('DB_HOST', $host);
+define('DB_USER', $user);
+define('DB_PASSWORD', $password);
+define('DB_NAME', $db);
+
+try {
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if ($conn->connect_error) {
+        die(json_encode(['error' => 'Erro na conexão: ' . $conn->connect_error]));
     }
+} catch (Exception $e) {
+    die(json_encode(['error' => 'Erro ao conectar: ' . $e->getMessage()]));
 }
-
-// Seu código de processamento normal aqui
-header('Content-Type: application/json');
 ?>
