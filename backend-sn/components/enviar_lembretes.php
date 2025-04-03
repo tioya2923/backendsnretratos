810db3a2807fd9fb3eb9b45ae6a623549e9582ed
Joao Dumba <30002076@students.ual.pt>
2025-04-03 01:52:40 +0100

    rmgjdaddlgdlfkkhkdgd

--- backend-sn/components/enviar_lembretes.php
+++ backend-sn/components/enviar_lembretes.php
@@ -19,7 +19,7 @@ function enviarLembretes()
     global $conn;
 
     // Carregar emails e nomes dos usuários que ainda não se inscreveram
-    $sql = "SELECT email, nome FROM usuarios WHERE id NOT IN (SELECT usuario_id FROM refeicoes WHERE data = CURDATE())";
+    $sql = "SELECT email, name FROM usuarios WHERE id NOT IN (SELECT usuario_id FROM refeicoes WHERE data = CURDATE())";
     $result = $conn->query($sql);
     $usuarios = [];
     if ($result && $result->num_rows > 0) {
