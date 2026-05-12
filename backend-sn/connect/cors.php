92d977a5ecef1516e0e70c831c50eac821178f83
Joao Dumba <72528227+tioya2923@users.noreply.github.com>
2026-05-12 09:56:24 +0100

    calendário11l5

--- backend-sn/connect/cors.php
+++ backend-sn/connect/cors.php
@@ -1,21 +1,22 @@
 <?php
 // Evitar envio de headers no CLI
 if (php_sapi_name() === 'cli') return;
-// Lista de origens permitidas
-$origensPermitidas = [
-    //'https://snrefeicoes.pt',
-    //'https://www.snrefeicoes.pt',
-    'https://snref-fronten-8dbe187fda6c.herokuapp.com',
-    'http://localhost:3000',
-];
 
-// Verifica se a origem da requisição está na lista de permitidas
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $origensPermitidas)) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+$permitida = (
+    $origin === 'http://localhost:3000' ||
+    str_ends_with($origin, '.herokuapp.com') ||
+    str_ends_with($origin, '.snrefeicoes.pt') ||
+    $origin === 'https://snrefeicoes.pt'
+);
+
+if ($origin && $permitida) {
+    header("Access-Control-Allow-Origin: " . $origin);
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    
+
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         exit(0); // Preflight request
     }
